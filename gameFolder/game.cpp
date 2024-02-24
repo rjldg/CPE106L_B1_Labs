f5f34e92728b6f3473b1f9765ef76daa8c6c906e
vboxuser <vboxuser@SoftDesLab.myguest.virtualbox.org>
2024-02-24 11:05:54 +0800

    Create extracted game folder

---
+++ gameFolder/game.cpp
@@ -0,0 +1,7 @@
+#include "game.h"
+#include "player.h"
+
+void Game::Play() {
+	Player P1;
+	P1.setName();
+}
---
+++ gameFolder/main.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+#include "game.h"
+
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	Game Game_obj;
+	Game_obj.Play();
+	return 0;
+}
