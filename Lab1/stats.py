46e51e90839a21d8fb7bc12693be200850d136da
vboxuser <vboxuser@SoftDesLab.myguest.virtualbox.org>
2024-03-02 10:14:24 +0800

    revised mean function

--- Lab1/stats.py
+++ Lab1/stats.py
@@ -6,15 +6,6 @@ def mean(l):
     ans = sum / len(l)
     return(ans)
     
-def median(arr):
-    arr = sorted(arr)
-    if(len(arr) == 0):
-        return 0
-    elif(len(arr) % 2 != 0):
-        return arr[int(len(arr) / 2)]
-    else:
-        return (arr[int(len(arr) / 2) - 1] + arr[int(len(arr) / 2)]) / 2
-    
 l = list(map(int, input("Enter list: ").split()))
 n = int(input("Enter 1 to get MODE, 2 to get MEAN, 3 to get MEDIAN: "))
 
