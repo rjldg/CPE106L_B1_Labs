d7f84ccb42abe3504c3354043f2c9ae33bbce624
vboxuser <vboxuser@SoftDesLab.myguest.virtualbox.org>
2024-03-02 10:17:36 +0800

    Polish code

--- Lab1/stats.py
+++ Lab1/stats.py
@@ -1,23 +1,6 @@
 def mode(l):
     return max(set(l), key=l.count)
 
-def mean(l):
-    sum = 0
-    for i in l:
-        sum += i
-    
-    ans = sum / len(l)
-    return(ans)
-    
-def median(arr):
-    arr = sorted(arr)
-    if(len(arr) == 0):
-        return 0
-    elif(len(arr) % 2 != 0):
-        return arr[int(len(arr) / 2)]
-    else:
-        return (arr[int(len(arr) / 2) - 1] + arr[int(len(arr) / 2)]) / 2
-
 l = list(map(int, input("Enter list: ").split()))
 n = int(input("Enter 1 to get MODE, 2 to get MEAN, 3 to get MEDIAN: "))
 
@@ -26,8 +9,8 @@ print(*l)
 
 funcs = {
     1: mode(l),
-    2: mean(l),
-    3: median(l)
+    # 2: mean(l),
+    # 3: median(l)
 }
 
 for index, func in funcs.items():
