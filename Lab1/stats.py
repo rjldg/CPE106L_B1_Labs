9dc3b66f70fa3b44c9e8cafceb3c4a49dc6b707e
vboxuser <vboxuser@SoftDesLab.myguest.virtualbox.org>
2024-03-02 09:49:15 +0800

    added mean func

--- Lab1/stats.py
+++ Lab1/stats.py
@@ -1,12 +1,27 @@
-
-
+def mean(l):
+    sum = 0
+    for i in l:
+        sum += i
+    
+    ans = sum / len(l)
+    return(ans)
+    
+def median(arr):
+    arr = sorted(arr)
+    if(len(arr) == 0):
+        return 0
+    elif(len(arr) % 2 != 0):
+        return arr[int(len(arr) / 2)]
+    else:
+        return (arr[int(len(arr) / 2) - 1] + arr[int(len(arr) / 2)]) / 2
+    
 l = list(map(int, input("Enter list: ").split()))
 n = int(input("Enter 1 to get MODE, 2 to get MEAN, 3 to get MEDIAN: "))
 
 print(*l)
 
 funcs = {
-    1: mode(l),
+    # 1: mode(l),
     2: mean(l),
     3: median(l)
 }
