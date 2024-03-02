ee72800d8fc47744d36eb14a6e1539e34180ae06
vboxuser <vboxuser@SoftDesLab.myguest.virtualbox.org>
2024-03-02 09:38:34 +0800

    Update again

--- Lab1/stats.py
+++ Lab1/stats.py
@@ -4,10 +4,16 @@ def mode(l):
 """
 def mean(l):
     print("lorem ipsum")
-
-def median(l):
-    print("lorem ipsum")
 """
+
+def median(arr):
+    arr = sorted(arr)
+    if(len(arr) == 0):
+        return 0
+    elif(len(arr) % 2 != 0):
+        return arr[int(len(arr) / 2)]
+    else:
+        return (arr[int(len(arr) / 2) - 1] + arr[int(len(arr) / 2)]) / 2
 
 l = list(map(int, input("Enter list: ").split()))
 n = int(input("Enter 1 to get MODE, 2 to get MEAN, 3 to get MEDIAN: "))
@@ -18,7 +24,7 @@ print(*l)
 funcs = {
     1: mode(l),
     # 2: mean(l),
-    # 3: median(l)
+    3: median(l)
 }
 
 for index, func in funcs.items():
