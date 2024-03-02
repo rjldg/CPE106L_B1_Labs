3eb379a51acef7fbe32788042dbe2f44e4de4697
rjldg <rjlimdeguzman@gmail.com>
2024-03-02 09:13:31 +0800

    Added stats.py with main code

---
+++ Lab1/stats.py
@@ -0,0 +1,19 @@
+
+
+l = list(map(int, input("Enter list: ").split()))
+n = int(input("Enter 1 to get MODE, 2 to get MEAN, 3 to get MEDIAN: "))
+
+print(*l)
+
+funcs = {
+    1: mode(l),
+    2: mean(l),
+    3: median(l)
+}
+
+for index, func in funcs.items():
+    if(index == n):
+        if(func == -1):
+            print("No Mode.")
+        else:
+            print(f"Ans: {func:.2f}")
