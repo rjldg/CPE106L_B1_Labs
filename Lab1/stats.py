fd6fbc388a16d683f634e0434dfbe0e3dfbc6333
vboxuser <vboxuser@SoftDesLab.myguest.virtualbox.org>
2024-03-02 09:34:36 +0800

    updated mode function

--- Lab1/stats.py
+++ Lab1/stats.py
@@ -1,21 +1,24 @@
 def mode(l):
     print(max(set(l), key=l.count))
 
+"""
 def mean(l):
     print("lorem ipsum")
 
 def median(l):
     print("lorem ipsum")
+"""
 
 l = list(map(int, input("Enter list: ").split()))
 n = int(input("Enter 1 to get MODE, 2 to get MEAN, 3 to get MEDIAN: "))
+
 
 print(*l)
 
 funcs = {
     1: mode(l),
-    2: mean(l),
-    3: median(l)
+    # 2: mean(l),
+    # 3: median(l)
 }
 
 for index, func in funcs.items():
