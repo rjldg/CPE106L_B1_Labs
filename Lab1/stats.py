5cc543d046660be4180f52b1a86f2424552c29b0
azra-dev <azraelanonas@gmail.com>
2024-03-09 21:54:42 +0800

    Error Handling of Statistical Treatment Inputs

--- Lab1/stats.py
+++ Lab1/stats.py
@@ -11,3 +11,20 @@ def mode(arr):
     return value
 
 arr = list(map(int, input("Enter a list of number separated by spaces: ").split()))
+
+stat_func_dict = {
+    "mean": mean(arr),
+    "median": median(arr),
+    "mode": mode(arr),
+}
+
+valid_status = False
+while not valid_status:
+    stat_treatment = input("Enter statistical treatment to be applied (mean, median, mode) : ")
+    for treatment, stat_func in stat_func_dict.items():
+        if treatment == str.lower(stat_treatment):
+            print(f"valid: {str.lower(stat_treatment)}")
+            valid_status = True
+            break
+    if not valid_status:
+        print("Invalid input.")
